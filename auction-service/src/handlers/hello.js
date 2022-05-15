037e0cbcb156a548b00eaff4ec3e7c4a5114ff1e
John Koen <jkoen@smartronix.com>
2022-05-15 09:53:33 -0400

    Cleaned up serverless config, added initial createAuction function

--- auction-service/src/handlers/hello.js
+++
@@ -1,8 +0,0 @@
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Welcome to the auction service!' }),
-  };
-}
-
-export const handler = hello;
