60fb0a8b27a9e6fab3a25c7a39c9a3e6de8979a0
John Koen <jkoen@smartronix.com>
2022-05-16 14:03:56 -0400

    get ended auctions function

--- auction-service/src/handlers/processAuctions.js
+++ auction-service/src/handlers/processAuctions.js
@@ -1,5 +1,8 @@
-function processAuctions(event, context) {
-  console.log('Processing Auctions!');
+import { getEndedAuctions } from '../lib/getEndedAuctions';
+
+async function processAuctions(event, context) {
+  const auctionsToClose = await getEndedAuctions();
+  console.log(auctionsToClose);
 }
 
 export const handler = processAuctions;
